1930ddda54f3a97209dacd5a3088d9ace66abe17
Chun Chow <erenyager213@gmail.com>
2021-07-30 01:08:30 +0800

    testing

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+
+
+router.get('/homepage', function(req, res, next) {
+  const p1 = 'This server uses a <a href="https://pugjs.org/api/getting-started.html" target="_blank">pug template</a> for the html output'
+  res.render('layout', { title: 'Hello World', p1 });
+});
+
+router.get('/abi', function(req, res, next) {
+  res.render('layout2');
 });
 
 module.exports = router;
